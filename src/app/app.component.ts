b5ee128bfcad08785ab17e8d5ac68a11662ee1dd
Marcel Richter <mail@mrclrchtr.com>
2017-07-19 23:31:38 +0200

    V1.0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,18 +13,50 @@ export class AppComponent implements OnInit {
   index = 0;
 
   questions: Map<number, string> = new Map([
-    [0, 'Welche Weinfarbe ist dein Favorit?'],
-    [1, 'Welchen Geschmack bevorzugst du?'],
-    [2, 'Wie süß magst du deinen Wein?']
+    [1, 'Welche Weinfarbe ist dein Favorit?'],
+    [2, 'Welchen Geschmack bevorzugst du?'],
+    [3, 'Wie süß magst du deinen Wein?']
   ]);
 
 
-  answers: Map<number, Map<number, string>> = new Map([
-      [0, new Map([[0, 'Rot'], [1, 'Weiß'], [2, 'Rose']])],
-      [1, new Map([[0, 'Leicht'], [1, 'Mittelmäßig'], [2, 'Kräftig']])],
-      [2, new Map([[0, 'Trocken'], [1, 'Halbtrocken'], [2, 'Lieblich']])]
+  answers: Map<number, Map<string, string>> = new Map([
+    [0, new Map([['0', 'Rot'], ['1', 'Weiß'], ['2', 'Rosé']])],
+    [1, new Map([['0', 'Leicht'], ['1', 'Mittelkräftig'], ['2', 'Kräftig']])],
+    [2, new Map([['0', 'Trocken'], ['1', 'Halbtrocken'], ['2', 'Lieblich']])]
     ]
   );
+
+  possibleResults: Map<string, string> = new Map([
+    ['000', 'http://wein-fuer-laien.de/?page_id=1493'],
+    ['001', 'http://wein-fuer-laien.de/?page_id=1493'],
+    ['010', 'http://wein-fuer-laien.de/?page_id=1493'],
+    ['011', 'http://wein-fuer-laien.de/?page_id=1493'],
+    ['002', 'http://wein-fuer-laien.de/?page_id=1493'],
+    ['020', 'http://wein-fuer-laien.de/?page_id=1493'],
+    ['022', 'http://wein-fuer-laien.de/?page_id=1493'],
+    ['012', 'http://wein-fuer-laien.de/?page_id=1493'],
+    ['021', 'http://wein-fuer-laien.de/?page_id=1493'],
+    ['111', 'http://wein-fuer-laien.de/?page_id=1493'],
+    ['101', 'http://wein-fuer-laien.de/?page_id=1493'],
+    ['110', 'http://wein-fuer-laien.de/?page_id=1493'],
+    ['111', 'http://wein-fuer-laien.de/?page_id=1493'],
+    ['102', 'http://wein-fuer-laien.de/?page_id=1493'],
+    ['120', 'http://wein-fuer-laien.de/?page_id=1493'],
+    ['122', 'http://wein-fuer-laien.de/?page_id=1493'],
+    ['112', 'http://wein-fuer-laien.de/?page_id=1493'],
+    ['121', 'http://wein-fuer-laien.de/?page_id=1493'],
+    ['122', 'http://wein-fuer-laien.de/?page_id=1493'],
+    ['211', 'http://wein-fuer-laien.de/?page_id=1493'],
+    ['201', 'http://wein-fuer-laien.de/?page_id=1493'],
+    ['210', 'http://wein-fuer-laien.de/?page_id=1493'],
+    ['211', 'http://wein-fuer-laien.de/?page_id=1493'],
+    ['202', 'http://wein-fuer-laien.de/?page_id=1493'],
+    ['220', 'http://wein-fuer-laien.de/?page_id=1493'],
+    ['222', 'http://wein-fuer-laien.de/?page_id=1493'],
+    ['212', 'http://wein-fuer-laien.de/?page_id=1493'],
+    ['221', 'http://wein-fuer-laien.de/?page_id=1493'],
+    ['222', 'http://wein-fuer-laien.de/?page_id=1493'],
+  ]);
 
   selection: Map<number, number> = new Map();
 
@@ -32,9 +64,9 @@ export class AppComponent implements OnInit {
     this.logger = _logger;
   }
 
-  select(index: number, selection: number) {
-    this.logger.info('Selection: ' + selection);
-    this.selection.set(index, selection);
+  select(index: number, currentSelection: number) {
+    this.logger.info('Selection: ' + currentSelection);
+    this.selection.set(index, currentSelection);
     if (index < (this.questions.size) - 1) {
       this.logger.info('Next question');
       this.index++;
@@ -42,7 +74,12 @@ export class AppComponent implements OnInit {
       this.logger.info('Result: ');
       this.selection.forEach((value, key) => {
         this.logger.info(key, value);
-      })
+      });
+
+      const resultString: string = '' + this.selection.get(0) + this.selection.get(1) + this.selection.get(2);
+      this.logger.info('Result:' + resultString);
+      this.logger.info('Redirect to:' + this.possibleResults.get(resultString));
+      window.top.location.href = this.possibleResults.get(resultString);
     }
   }
 
